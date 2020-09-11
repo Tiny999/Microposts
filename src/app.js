3491a647da3554dd719d0a2433e8518f34c14306
Tiny999 <salamiabbas3@gmail.com>
2020-09-11 23:13:57 +0100

    update-posts

--- src/app.js
+++ src/app.js
@@ -11,6 +11,12 @@ document.querySelector('.post-submit').addEventListener('click', submitPost);
 // Delete Post Event
 document.querySelector('#posts').addEventListener('click', deletePosts);
 
+// Edit State Post Event
+document.querySelector('#posts').addEventListener('click', enableEdit)
+
+// Cancel Edit event
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 // Get Posts from server
 function getPosts(){
   http.get('http://localhost:3000/posts')
@@ -19,24 +25,49 @@ function getPosts(){
 }
 
 // Add Posts
-function submitPost(e){
+function submitPost(){
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
 
-  const data = {
-    title,
-    body
+  // Validate Inputs
+  if(title === '' || body === ''){
+    ui.showAlert('Please fill in all fields', 'alert alert-danger');
+  } else{
+    const data = {
+      title,
+      body
+    }  
+
+    // Check for id
+    if(id === ''){
+
+      // Create Post 
+      http.post('http://localhost:3000/posts', data)
+      .then(data => {
+        ui.showAlert('Post added', 'alert alert-success');
+        ui.clearFields();
+
+        getPosts();
+      })
+      .catch(err => console.log(err));
+
+    }else{
+
+      // Update Post
+      http.put(`http://localhost:3000/posts/${id}`, data)
+      .then(data => {
+        ui.showAlert('Post Updated', 'alert alert-success');
+        ui.changeFormState('add');
+
+        getPosts();
+      })
+      .catch(err => console.log(err));
+
+    }
+    
+    
   }
-
-  // Create Post 
-  http.post('http://localhost:3000/posts', data)
-  .then(data => {
-    ui.showAlert('Post added', 'alert alert-success');
-    ui.clearFields()
-
-    getPosts();
-  })
-  .catch(err => console.log(err));
 };
 
 // Delete Posts
@@ -55,4 +86,31 @@ function deletePosts(e){
   }
 
   e.preventDefault();
+}
+
+// Enable edit
+function enableEdit(e){
+  if(e.target.parentElement.classList.contains('edit')){
+    const id = e.target.parentElement.dataset.id;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+
+    const data = {
+      id,
+      title, 
+      body
+    }
+
+    // Fill form with post to be edited
+    ui.fillForm(data);
+  }
+}
+
+// Cancel Edit state
+function cancelEdit(e){
+  if(e.target.classList.contains('post-cancel')){
+    ui.changeFormState('add');
+
+    e.preventDefault();
+  }
 }
--- src/ui.js
+++ src/ui.js
@@ -65,6 +65,55 @@ class UI{
     this.bodyInput.value = '';
   }
 
+  fillForm(data){
+    this.titleInput.value = data.title;
+    this.bodyInput.value = data.body;
+    this.idInput.value = data.id;
+
+    this.changeFormState('edit');
+  }
+
+  clearIdInput(){
+    this.idInput.value = '';
+  }
+
+  changeFormState(type){
+    if(type === 'edit'){
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+      // Create Cancel Button
+      const button = document.createElement('button');
+
+      button.className = 'post-cancel btn btn-light btn-block';
+      button.appendChild(document.createTextNode('Cancel Edit'));
+
+      // Get Parent
+      const cardForm = document.querySelector('.card-form');
+
+      // Get element to insert before
+      const formEnd = document.querySelector('.form-end');
+
+      cardForm.insertBefore(button, formEnd);
+
+    } else{
+      this.postSubmit.textContent = 'Post It';
+      this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+
+      // remove cancel button if there
+      if(document.querySelector('.post-cancel')){
+        document.querySelector('.post-cancel').remove();
+      }
+
+      // clear id input 
+      this.clearIdInput;
+
+      // clear text fields 
+      this.clearFields();
+    }
+
+  }
+
 }
 
 export const ui = new UI();
